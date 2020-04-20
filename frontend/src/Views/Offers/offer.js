f32601861376afc02cccea185fe67eed332d2d2b
tomMisson <11tmisson@gmail.com>
2020-04-20 16:30:38 +0100

    Add hidden img render for future implementaion

--- frontend/src/Views/Offers/offer.js
+++ frontend/src/Views/Offers/offer.js
@@ -5,6 +5,7 @@ export default class offer extends Component {
     render() {
         return (
             <div>
+                {/* <img alt="offer" src={this.props.img}/> */}
                 <span><h3>{this.props.name}</h3> - <em>{this.props.dist}</em> </span>
                 <p>{this.props.desc}</p>
             </div>
