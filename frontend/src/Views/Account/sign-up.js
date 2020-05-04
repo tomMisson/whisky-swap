865429c1d3a28c72d14bc2bd10313593a9f81719
tomMisson <11tmisson@gmail.com>
2020-05-04 15:01:12 +0100

    Add image upload feild to frontend

--- frontend/src/Views/Account/sign-up.js
+++ frontend/src/Views/Account/sign-up.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import hash from 'hash.js'
+import axios from 'axios'
 
 export default class profile extends Component {
 
@@ -30,14 +31,21 @@ export default class profile extends Component {
             case "addrLn1":
                 this.setState({line1Address: event.target.value});
                 break;
+            case "addrLn2":
+                this.setState({line2Address: event.target.value});
+                break;
+            case "addrLn3":
+                this.setState({line3Address: event.target.value});
+                break;
             case "postcode":
                 this.setState({postcode: event.target.value});
                 break;
             case "deliveryOption":
                 this.setState({deliveryOption: event.target.value});
                 break;
-            case "url":
-                this.setState({url: event.target.value});
+            case "profPic":
+                this.setState({uploadProgress:0})
+                this.setState({file: event.target.files[0]});
                 break;
             default:
         }
@@ -46,43 +54,40 @@ export default class profile extends Component {
     async handleSubmit(event) {
         event.preventDefault();
 
-        const requestOptions = {
-            crossDomain:true,
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin':true},
-            body: JSON.stringify(
+        var fd = new FormData();
+        fd.append("data", JSON.stringify(
+            {
+                "name":this.state.name,
+                "email": this.state.email,
+                "pswd": hash.sha256().update(this.state.pswd).digest('hex'),
+                "phone": this.state.phone,
+                "address1": this.state.line1Address,
+                "address2": this.state.line2Address,
+                "address3": this.state.line3Address,
+                "postcode" : this.state.postcode,
+                "delivery": this.state.deliveryOption
+            }))
+        fd.append("profPic", this.state.file)
+
+        axios.post(process.env.REACT_APP_API_URL.concat("/profiles"), fd,
+        {
+            onUploadProgress: progressEvent => {
+                this.setState({uploadProgress: Math.round((progressEvent.loaded/progressEvent.total) *100)})
+            }
+        })
+            .then(res => {
+                if(res.data ===409)
+                    alert("A user with this email already exisits, if you belive this is an error, please contact support.")
+                else if(res.data !==409)
                 {
-                    "name":this.state.name,
-                    "email": this.state.email,
-                    "pswd": hash.sha256().update(this.state.pswd).digest('hex'),
-                    "phone": this.state.phone,
-                    "address line 1": this.state.line1Address,
-                    "postcode" : this.state.postcode,
-                    "delivery": this.state.deliveryOption
+                    console.log(res)
+                    sessionStorage.setItem("UID", res.data.UID)
+                    sessionStorage.setItem("loggedIn", true)
+                    alert("Signed up!")
+                    window.location.replace(process.env.REACT_APP_APP_URL+"/account")
                 }
-            )
-        };
-        var res = await fetch(process.env.REACT_APP_API_URL.concat("/profiles"), requestOptions)
-        res = await res.json()
-        console.log(res)
-        if(res === 409)
-        {
-            alert("There is already an account registered with that email address. If you belive this isn't the case, please contact support")
-            window.location.replace(process.env.REACT_APP_APP_URL+"/sign-in")
-        }
-        else if(res !== 409)
-        { 
-            console.log(res)
-            sessionStorage.setItem("UID", res.UID)
-            sessionStorage.setItem("loggedIn", true)
-            alert("Signed up!")
-            window.location.replace(process.env.REACT_APP_APP_URL+"/account")
-        }
-        else
-        {
-            alert("Encountered an unexpected error");
-        }
-    
+            })
+            .catch(err => alert("Unexpercted error: "+err))
     }
 
     toggleSignUp(event){
@@ -119,35 +124,48 @@ export default class profile extends Component {
                     <label htmlFor="preferedDelivery">
                         Prefered delivery option:
                         <select required name="preferedDelivery" id="deliveryOption" value={this.state.deliveryOption} onChange={this.handleForm}>
-                            <option defaultValue="Collection">In Person Collection</option>
-                            <option value="Delivery">In Person Delivery</option>
-                            <option value="Post">Post It!</option>
+                            <option defaultValue=""></option>
+                            <option value="Collection">I'll collect it</option>
+                            <option value="Delivery">Please bring it to me</option>
+                            <option value="Post">Post it to me</option>
                         </select>
                     </label>
                     <br/>
                     {
-                        this.state.deliveryOption === "Collection"?
-                        null:<>
-                        <label htmlFor="addrLn1">
-                            Address Line 1:
-                            <input type="text" name="addrLn1" id="addrLn1" value={this.state.line1Address} onChange={this.handleForm}/>
-                        </label>
-                        <br/>
-                        <label htmlFor="postcode">
-                            Postcode:
-                            <input type="text" name="postcode" id="postcode" value={this.state.postcode} onChange={this.handleForm}/>
-                        </label>
+                        this.state.deliveryOption !== "Collection"?
+                        <>
+                            <label htmlFor="addrLn1">
+                                House name/number:
+                                <input type="text" name="addrLn1" id="addrLn1" value={this.state.line1Address} onChange={this.handleForm}/>
+                            </label>
+                            <br/>
+                            <label htmlFor="addrLn2">
+                                Street name:
+                                <input type="text" name="addrLn2" id="addrLn2" value={this.state.line2Address} onChange={this.handleForm}/>
+                            </label>
+                            <br/>
+                            <label htmlFor="addrLn3">
+                                Post town:
+                                <input type="text" name="addrLn3" id="addrLn3" value={this.state.line3Address} onChange={this.handleForm}/>
+                            </label>
+                            <br/>
+                            <label htmlFor="postcode">
+                                Postcode:
+                                <input type="text" name="postcode" id="postcode" value={this.state.postcode} onChange={this.handleForm}/>
+                            </label>
                         <br/>
                         </>
+                        :null
+                        
                     }
-                    <label htmlFor="url">
-                        URL to profile pic:
-                        <input type="url" name="url" id="url" value={this.state.url} onChange={this.handleForm}/>
+                    <label htmlFor="pic">
+                        Profile picture:
+                        <input type="file" accept="image/*" name="pic" id="profPic" onChange= {this.handleForm} />
                     </label>
                     <br/>
                     <input type="submit" value="Submit"/>
                 </form>
-                <a href="/">Back</a>
+                <a href="/sign-in">Back</a>
             </main>
         )
     }
