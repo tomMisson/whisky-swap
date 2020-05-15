b4790d066bc8a854170a12ba3c5f602f22e7c9d4
tomMisson <11tmisson@gmail.com>
2020-05-15 21:43:00 +0100

    Remove log line and add loader

--- frontend/src/Views/Account/reset-password.js
+++ frontend/src/Views/Account/reset-password.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import hash from 'hash.js'
 import cookie from 'react-cookies'
+import Loader from '../../Components/Loader'
 
 export default class resetPassword extends Component {
 
@@ -33,6 +34,7 @@ export default class resetPassword extends Component {
     handleSubmit = async (e) => {
         e.preventDefault()
 
+        this.setState({waiting:true})
         if(this.props.uid !== undefined)
         {
             const requestOptions = {
@@ -45,10 +47,12 @@ export default class resetPassword extends Component {
                 var response = await fetch(process.env.REACT_APP_API_URL.concat("/profiles-password/"+this.props.uid), requestOptions);
                 if(response.status===200)
                 {
+                    this.setState({waiting:false})
                     alert("Updated password")
                     window.location.replace(process.env.REACT_APP_APP_URL+"/account")
                 }
                 else if(response.status===304){
+                    this.setState({waiting:false})
                     alert("Password wasn't changed")
                     window.location.replace(process.env.REACT_APP_APP_URL+"/account")
                 }
@@ -58,7 +62,7 @@ export default class resetPassword extends Component {
             }       
         }
 
-        else if(!this.state.loggedIn){
+        if(!this.state.loggedIn){
             if(this.state.conf === this.state.newPassword){
                 const requestOptions = {
                     crossDomain: true,
@@ -67,63 +71,72 @@ export default class resetPassword extends Component {
                     body: JSON.stringify({password:hash.sha256().update(this.state.newPassword).digest('hex')})
                 };
                 try{
-                    var response = await fetch(process.env.REACT_APP_API_URL.concat("/profiles-password/"+cookie.load("UID")), requestOptions);
+                    response = await fetch(process.env.REACT_APP_API_URL.concat("/profiles-password/"+cookie.load("UID")), requestOptions);
                     if(response.status===200)
                     {
+                        this.setState({waiting:false})
                         alert("Updated password")
                         window.location.replace(process.env.REACT_APP_APP_URL+"/account")
                     }
                 }
                 catch(err){ 
+                    this.setState({waiting:false})
                     alert("Something went wrong: " + err)
                 }
             }
             else{
+                this.setState({waiting:false})
                 alert("Passwords don't match, please check them and try again")
             }
         }
         else{
             await fetch(process.env.REACT_APP_API_URL+"/send-email-pswd/"+this.state.email)
-                .then(res=> {
+                .then(res => {
+                    
                     if(res.status === 200){
+                        this.setState({waiting:false})
                         alert("We have sent you an email!")
+                        window.location.replace(process.env.REACT_APP_APP_URL+"/")
                     }
-                    else
-                        alert("No account found with that email address")
-            })
+                })
+                .catch(err => console.log(err))
         }
     }
 
     render() {
-        return (
-            this.props.uid === undefined ?
-                !this.state.loggedIn ?
-                <main>
-                    <p>Enter the password you would like to use:</p>
-                    <form onSubmit={this.handleSubmit}>
-                        <label htmlFor="pswd" >New password: <input id="pswd" name="pswd" type="password" value={this.state.newPassword} onChange={this.handleFormData}/></label><br/>
-                        <label htmlFor="pswdConf" >Confirm password: <input id="pswdConf" name="pswdConf" type="password" value={this.state.conf} onChange={this.handleFormData}/></label>
-                        <br/>
-                        <button type="submit">Update</button><br/>
-                    </form>
-                    <button onClick={this.back}>Back</button>
-                </main>
-                :
-                <main>
-                    <p>Please enter your email address and we will send you an email to update your password:</p>
-                    <form onSubmit={this.handleSubmit}>
-                        <input type="email" id="email" onChange={this.handleFormData}/><br/>
-                        <button type="submit">Send me an email</button>
-                    </form>
-                </main>
+        return(
+        
+        this.state.waiting ?
+        <Loader/>
+        :
+        this.props.uid === undefined ?
+            !this.state.loggedIn ?
+            <main>
+                <p>Enter the password you would like to use:</p>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="pswd" >New password: <input id="pswd" name="pswd" type="password" value={this.state.newPassword} onChange={this.handleFormData}/></label><br/>
+                    <label htmlFor="pswdConf" >Confirm password: <input id="pswdConf" name="pswdConf" type="password" value={this.state.conf} onChange={this.handleFormData}/></label>
+                    <br/>
+                    <button type="submit">Update</button><br/>
+                </form>
+                <button onClick={this.back}>Back</button>
+            </main>
             :
             <main>
-                <p>Enter your new password:</p>
+                <p>Please enter your email address and we will send you an email to update your password:</p>
                 <form onSubmit={this.handleSubmit}>
-                    <input type="password" id="pswd" onChange={this.handleFormData}/><br/>
-                    <button type="submit">Update password</button>
+                    <input type="email" id="email" onChange={this.handleFormData}/><br/>
+                    <button type="submit">Send me an email</button>
                 </form>
             </main>
+        :
+        <main>
+            <p>Enter your new password:</p>
+            <form onSubmit={this.handleSubmit}>
+                <input type="password" id="pswd" onChange={this.handleFormData}/><br/>
+                <button type="submit">Update password</button>
+            </form>
+        </main>
         )
     }
 }
