de705fa97a6914920fd4cde1ab7424bfd5634615
tomMisson <11tmisson@gmail.com>
2020-05-19 01:28:33 +0100

    Add statement about the site

--- frontend/src/Views/home.js
+++ frontend/src/Views/home.js
@@ -6,8 +6,7 @@ export default class home extends Component {
         return (
             <main>
                 <h2>Welcome to Doorstep drams!</h2>
-                <p>A site about sharing whisky</p>
-                <p>Got a bottle you think is worth sharing? Want to try something different? <Link to="/browse">Browse offers now!</Link></p>
+                
             </main>
         )
     }
