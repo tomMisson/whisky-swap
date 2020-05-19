1013b836534fb8329ea02cc621e5db52066b1f66
tomMisson <11tmisson@gmail.com>
2020-05-19 01:36:16 +0100

    Restructure home

--- frontend/src/Views/home.js
+++ frontend/src/Views/home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import AvalibleTrades from './Offers/avalibleTrades'
 
 export default class home extends Component {
     render() {
         return (
             <main>
                 <h2>Welcome to Doorstep drams!</h2>
-                
+                <p>A site by whisky lovers for whisky lovers! We wanted a way to connect people (and drams!) together and share what people have in their cupboards, cabinets (if you are posh), shelves and sides to experience something different. Whisky is for drinking and for sharing, so why not give it a go and find your next favourite tipple. </p>
+                <h2>Avalible for trading</h2>
+                <AvalibleTrades/>
             </main>
         )
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -55,10 +55,7 @@ ReactDOM.render(
         )}/>
         <Route exact={true} path='/' render={() => (
           <div className="App">
-            <Home />
-            <p>A site by whisky lovers for whisky lovers! We wanted a way to connect people (and drams!) together and share what people have in their cupboards, cabinets (if you are posh), shelves and sides to experience something different. Whisky is for drinking and for sharing, so why not give it a go and find your next favourite tipple. </p>
-            <h2>Avalible for trading</h2>
-            <AvalibleTrades/>
+            <Home/>
           </div>
         )}/>
         <Route path='/offer/' render={() => (
