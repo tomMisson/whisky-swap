ba01f40100b5554314b76dc832f037be985454d2
tomMisson <11tmisson@gmail.com>
2020-05-19 20:35:22 +0100

    Styled account page

--- frontend/src/Views/Account/account.js
+++ frontend/src/Views/Account/account.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from '../../Components/Loader'
 import cookie from 'react-cookies'
+import './account.css'
 
 export default class account extends Component {
 
@@ -46,35 +47,39 @@ export default class account extends Component {
         return (
             cookie.load("loggedIn")?
             this.state.waiting? 
+            
                 <Loader/>
                 :
                 cookie.load("UID") !== undefined?
-                <main>
-                    {this.state.user.img !== undefined ?<img style={{borderRadius:"25px"}} alt="profilePic" width="100" src={this.state.user.img}/>:null}
-                    <h1> Hello, {this.state.fname}!</h1>
-                    {this.state.user.image === undefined ? null:<img style={{borderRadius: "25px"}} alt="Profile" src=""/>}
-                    <h2>Basic details</h2>
-                    <ul>
-                        <li>Name:<p>{this.state.user.name}</p></li>
-                        <li>Email address:<p>{this.state.user.email}</p>{this.state.user.verifiedEmail? null : <span><p className="warning">You need to verify your email before you can request a trade</p><button onClick={this.sendEmail}>Resend verification email</button></span>}</li>
-                        <li>Phone number:<p>{this.state.user.phone}</p></li>
-                    </ul>
-                    <ul>
-                        <li><Link to="update-details">Update details</Link></li>
-                        <li><Link to="update-password">Change your password</Link></li>
-                        <li><button onClick={this.handleSignOut}>Not {this.state.fname}? Log out</button></li>
-                    </ul>
-                    <h2>Delivery preferences</h2>
-                    <ul>
-                        <li>Prefered Delivery: {this.state.user.delivery}</li>
-                        {
-                            this.state.user.delivery !== "Collection" ?
-                            <li>Delivery address: <p>{this.state.user.address1!==undefined || this.state.user.address1!==null? this.state.user.address1 :null}{this.state.user.address2!==undefined|| this.state.user.address2!==null? ", "+this.state.user.address2 :null}{this.state.user.address3!==undefined|| this.state.user.address3!==null? ", "+this.state.user.address3 :null} {this.state.user.postcode!==undefined || this.state.user.postcode!==null? ", "+this.state.user.postcode :null}</p></li>
-                            :
-                            <li><p>You have said you will collect</p></li>
-                        }
-                        <li><Link to="update-delivery">Update preferences</Link></li>
-                    </ul>
+                <main className="account">
+                    <div id="personal">
+                        {this.state.user.img !== undefined ?<img style={{borderRadius:"25px"}} alt="profile" width="100" src={this.state.user.img}/>:null}
+                        <h1> Hello, {this.state.fname}!</h1>
+                        <h2>Basic details</h2>
+                        <ul>
+                            <li>Name:<p>{this.state.user.name}</p></li>
+                            <li>Email address:<p>{this.state.user.email}</p>{this.state.user.verifiedEmail? null : <span><p className="warning">You need to verify your email before you can request a trade</p><button onClick={this.sendEmail}>Resend verification email</button></span>}</li>
+                            <li>Phone number:<p>{this.state.user.phone}</p></li>
+                        </ul>
+                        <ul>
+                            <li><Link to="update-details">Update details</Link></li>
+                            <li><Link to="update-password">Change your password</Link></li>
+                            <li><button onClick={this.handleSignOut}>Not {this.state.fname}? Log out</button></li>
+                        </ul>
+                    </div>
+                    <div id="delivery">
+                        <h2>Delivery preferences</h2>
+                        <ul>
+                            <li>Prefered Delivery: <p>{this.state.user.delivery}</p></li>
+                            {
+                                this.state.user.delivery !== "Collection" ?
+                                <li>Delivery address: <p>{this.state.user.address1!==undefined || this.state.user.address1!==null? this.state.user.address1 :null}{this.state.user.address2!==undefined|| this.state.user.address2!==null? ", "+this.state.user.address2 :null}{this.state.user.address3!==undefined|| this.state.user.address3!==null? ", "+this.state.user.address3 :null} {this.state.user.postcode!==undefined || this.state.user.postcode!==null? ", "+this.state.user.postcode :null}</p></li>
+                                :
+                                <li><p>You have said you will collect</p></li>
+                            }
+                            <li><Link to="update-delivery">Update preferences</Link></li>
+                        </ul>
+                    </div>
                 </main>
                 :
                 window.location.replace("/sign-in")
