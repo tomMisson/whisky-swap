2aed8822e85daee88c5a031dedf99931a694e699
tomMisson <11tmisson@gmail.com>
2020-05-23 23:24:20 +0100

    Create overlay component

---
+++ frontend/src/Components/overlay.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './overlay.css'
+
+export default class overlay extends Component {
+    render() {
+        return (
+            <div id="overlay">
+                
+            </div>
+        )
+    }
+}
