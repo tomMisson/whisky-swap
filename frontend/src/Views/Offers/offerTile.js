62816e0530686c364bbbc8454411ab035cfcfe22
tomMisson <11tmisson@gmail.com>
2020-05-27 17:21:50 +0100

    Added onclick handler

--- frontend/src/Views/Offers/offerTile.js
+++ frontend/src/Views/Offers/offerTile.js
@@ -5,7 +5,7 @@ export default class offerTile extends Component {
 
     render() {
         return (
-            <div className="offer">
+            <div className="offer" key={this.props.id} onClick={this.props.onHandleClick}>
                 {this.props.img === undefined ? null : <img alt="offer" width="120" src={this.props.img}/>}
                 <span><h3>{this.props.name}</h3> - <em>{this.props.dist}</em> {this.props.abv !== undefined ? <p>{this.props.abv}%</p> : null}</span>
                 <p>{this.props.desc}</p>
